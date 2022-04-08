c5b3806de81a90e2666d1ae5ba1178c1a447d225
neroxqs <99805602+neroxqs@users.noreply.github.com>
2022-04-09 00:20:41 +0300

    Update functions.js

--- ElfGame Staking Page/functions.js
+++ ElfGame Staking Page/functions.js
@@ -406,9 +406,9 @@ async function load(){
   window.stakeContract = await loadStakingContract();
   window.manaContract = await loadManaContract();
 
-  await loadTokens();
-  updateMana();
-  updateTotalStakedElfs();
+  //await loadTokens();
+  //updateMana();
+  //updateTotalStakedElfs();
 }
 
 // Onclick functions
