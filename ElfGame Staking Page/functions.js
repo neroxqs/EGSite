d65002275a157b803eb8b5f953d30409deee99f2
neroxqs <99805602+neroxqs@users.noreply.github.com>
2022-04-10 03:23:37 +0200

    Update functions.js

--- ElfGame Staking Page/functions.js
+++ ElfGame Staking Page/functions.js
@@ -196,7 +196,7 @@ async function rewards() {
     totalMana = 0;
     manaTaxPerOrc = await window.stakeContract.methods.manaTaxPerOrc().call()
 
-    stakedElfsArray.forEach(async function(id){
+    await stakedElfsArray.forEach(async function(id){
       var elf = stakedElfsInfo.get(id);
       var rewardElf = await window.stakeContract.methods.calculateReward(elf).call();
 
@@ -207,7 +207,9 @@ async function rewards() {
       totalMana += rewardElf;
 
       rewardAmountElf.innerHTML = rewardElf.toFixed(2);
-    }updateTotalElfRewards(totalMana.toFixed(2)); );
+    });
+    
+    updateTotalElfRewards(totalMana.toFixed(2));
 
     stakedOrcsArray.forEach(function(id){
       var rewardAmountOrc = document.getElementById('rewardTokensAmount'+id);
