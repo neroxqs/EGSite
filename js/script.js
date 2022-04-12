ba24b7f1f67584663c84ded5e5c0441b51a3a480
neroxqs <99805602+neroxqs@users.noreply.github.com>
2022-04-12 18:56:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ async function mintWithMana(){
   //const price = await window.mintContract.methods.manaPrice(numberBox.value);
   
   var number = (100*10**18)*numberBox.value;
-  const price = number.toString(16);
+  const price = web3.utils.toHex(number);
 
   await window.manaContract.methods.approve(json.mintContractAddress, price).send({ from: accounts[0] });
   await window.mintContract.methods.buyWithMana(numberBox.value).send({ from: accounts[0] });
